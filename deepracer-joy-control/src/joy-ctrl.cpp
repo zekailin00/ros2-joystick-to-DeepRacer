a8f1dd4962acf53cfe39e66686bea23556211cbe
zekai lin <zekailin00@gmail.com>
2022-06-15 17:04:54 -0400

    added control mappings

--- deepracer-joy-control/src/joy-ctrl.cpp
+++ deepracer-joy-control/src/joy-ctrl.cpp
@@ -19,7 +19,7 @@ class DeepRacerJoyControl : public rclcpp::Node
 {
   public:
     DeepRacerJoyControl()
-    : Node("joy_ctrl"), count_(0)
+    : Node("joy_ctrl"), count_(0), sensitivity(0.2)
     {
       publisher_ = this->create_publisher<deepracer_interfaces_pkg::msg::ServoCtrlMsg>("/ctrl_pkg/servo_msg", 10);
       timer_ = this->create_wall_timer(500ms, std::bind(&DeepRacerJoyControl::timer_callback, this));
@@ -30,23 +30,40 @@ class DeepRacerJoyControl : public rclcpp::Node
   private:
     void get_joy_ctrl(const sensor_msgs::msg::Joy::SharedPtr joy_ctrl)
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%f'", joy_ctrl->axes[1]);
+	    if (joy_ctrl->axes[1] > sensitivity || joy_ctrl->axes[1] < -sensitivity ||
+		joy_ctrl->axes[0] > sensitivity || joy_ctrl->axes[0] < -sensitivity)
+	    {
+		    auto servoMsg = deepracer_interfaces_pkg::msg::ServoCtrlMsg();  
+		    servoMsg.angle = joy_ctrl->axes[0];
+		    servoMsg.throttle = joy_ctrl->axes[1]; 
+		    RCLCPP_INFO(this->get_logger(), "Active Angle: '%f'", servoMsg.angle); 
+		    RCLCPP_INFO(this->get_logger(), "Active Throttle: '%f'", servoMsg.throttle); 
+		    publisher_->publish(servoMsg); 
+	    }
+	    else
+	    {
+		   auto servoMsg = deepracer_interfaces_pkg::msg::ServoCtrlMsg(); 
+       		   servoMsg.angle = 0.0f;  		   
+		   servoMsg.throttle = 0.0f;    
+		   publisher_->publish(servoMsg);
+	    }
     }
     
     void timer_callback()
     {
       count_++;
-      auto servoMsg = deepracer_interfaces_pkg::msg::ServoCtrlMsg();
-      servoMsg.angle = (((count_%2) == 0)? -1.0:1.0);
-      servoMsg.throttle = 0.1;
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%f'", servoMsg.angle);
-      publisher_->publish(servoMsg);
+      //auto servoMsg = deepracer_interfaces_pkg::msg::ServoCtrlMsg();
+      //servoMsg.angle = (((count_%2) == 0)? -1.0:1.0);
+      //servoMsg.throttle = 0.1;
+      //RCLCPP_INFO(this->get_logger(), "Publishing: '%f'", servoMsg.angle);
+      //publisher_->publish(servoMsg);
     }
     
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
     rclcpp::Publisher<deepracer_interfaces_pkg::msg::ServoCtrlMsg>::SharedPtr publisher_;
     size_t count_;
+    float sensitivity;
 };
 
 int main(int argc, char * argv[])
